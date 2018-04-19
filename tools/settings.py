a66a073a2f7d8028a6a5e34b7e3b6f0384a44b65
nschaetti <n.schaetti@gmail.com>
2018-04-19 17:56:57 +0200

    Settings

---
+++ tools/settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#
+
+# Imports
+from torchlanguage import transforms as ltransforms
+from torchvision import transforms
+
+
+################
+# Settings
+################
+
+# Settings
+lang_models = {'en': 'en_vectors_web_lg', 'fr': 'fr_core_news_md', 'es': 'es_core_news_md'}
