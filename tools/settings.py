37bb9f8de08d02a6ea517f4089a58b41425fbf69
nschaetti <n.schaetti@gmail.com>
2018-05-08 18:37:21 +0200

    Parameters and settings

--- tools/settings.py
+++ tools/settings.py
@@ -10,5 +10,48 @@ from torchvision import transforms
 # Settings
 ################
 
+# Parameters
+spectral_radius = 0.95
+input_sparsity = 0.1
+w_sparsity = 0.1
+input_scaling = 0.5
+n_test = 20
+n_samples = 5000
+leaky_rate = 0.1
+reservoir_size = 400
+
 # Settings
-lang_models = {'en': 'en_vectors_web_lg', 'fr': 'fr_core_news_md', 'es': 'es_core_news_md'}
+lang_models = {
+    'en': 'en_vectors_web_lg',
+    'fr': 'fr_core_news_md',
+    'it': '~/Projets/TURING/Datasets/fasttext/wiki.it/wiki.it.vec',
+    'sp': '~/Projets/TURING/Datasets/fasttext/wiki.es/wiki.es.vec',
+    'pl': '~/Projets/TURING/Datasets/fasttext/wiki.pl/wiki.pl.vec'
+}
+
+lang_spacy_models = {
+    'en': 'en_vectors_web_lg',
+    'fr': 'fr_core_news_md',
+    'it': 'nltk',
+    'sp': 'es_core_news_md',
+    'pl': 'nltk'
+}
+
+
+lang_models_dim = {
+    'en': 300,
+    'fr': 300,
+    'it': 300,
+    'sp': 300,
+    'pl': 300,
+}
+
+
+lang_models_lang = {
+    'en': 'en',
+    'fr': 'french',
+    'it': 'italian',
+    'sp': 'spanish',
+    'pl': 'polish',
+}
+
