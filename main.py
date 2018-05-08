a2a05c8fb652effd30564e86d2c00bc0a7db94ba
nschaetti <n.schaetti@gmail.com>
2018-05-08 19:30:47 +0200

    Main program

--- main.py
+++ main.py
@@ -22,23 +22,15 @@
 # Imports
 import torch.utils.data
 import dataset
+from tools import settings
 import echotorch.nn as etnn
 from torch.autograd import Variable
-import echotorch.utils
 import torchlanguage.transforms as transforms
-import matplotlib.pyplot as plt
 import tools.functions
 import tools.settings
-import numpy as np
 import os
+import echotorch
 
-# Experiment settings
-reservoir_size = 1000
-spectral_radius = 0.95
-input_sparsity = 0.1
-w_sparsity = 0.1
-input_scaling = 0.5
-leak_rate = 0.01
 
 # Argument
 args = tools.functions.argument_parser_execution()
@@ -46,135 +38,137 @@ args = tools.functions.argument_parser_execution()
 # Dataset info
 data_infos = tools.functions.data_info(args.input_dataset)
 
-# For each lang
-for lang in ['en']:
+# Collection info
+collection_info = dataset.TIRAAuthorIdentificationDataset.collection_infos(args.input_dataset)
+
+# For each problem
+for problem_description in collection_info:
+    # Problem name
+    problem_name = problem_description['problem-name']
+    problem_lang = problem_description['lang']
+    problem_encoding = problem_description['encoding']
+
     # Transformer
-    transformer = transforms.Compose([
-        transforms.RemoveLines(),
-        transforms.GloveVector(model=tools.settings.lang_models[lang])
-    ])
+    if args.lang == 'en' or args.lang == 'fr':
+        transformer = transforms.Compose([
+            # transforms.RemoveLines(),
+            transforms.GloveVector(model=tools.settings.lang_models[problem_lang])
+        ])
+    else:
+        transformer = transforms.Compose([
+            # transforms.RemoveLines(),
+            transforms.Token(model=tools.settings.lang_spacy_models[problem_lang],
+                             lang=tools.settings.lang_models_lang[problem_lang]),
+            transforms.GensimModel(model_path=tools.settings.lang_models[problem_lang])
+        ])
+    # end if
 
-    #  For each problem
-    for problem in np.arange(1, data_infos[lang]+1):
-        # Author identification training dataset
-        pan18loader_training = torch.utils.data.DataLoader(
-            dataset.AuthorIdentificationDataset(
-                root=args.input_dataset,
-                download=True,
-                transform=transformer,
-                problem=problem,
-                lang=lang
-            ),
-            batch_size=1, shuffle=True
-        )
+    # Author identification training dataset
+    pan18loader_training = torch.utils.data.DataLoader(
+        dataset.TIRAAuthorIdentificationDataset(
+            root=args.input_dataset,
+            transform=transformer,
+            problem_name=problem_name,
+            encoding=problem_encoding,
+            train=True
+        ),
+        batch_size=1, shuffle=True
+    )
 
-        # Author identification unknown dataset
-        pan18loader_unknown = torch.utils.data.DataLoader(
-            dataset.AuthorIdentificationDataset(
-                root=args.input_dataset,
-                download=True,
-                transform=transformer,
-                problem=problem,
-                train=False,
-                lang=lang
-            ),
-            batch_size=1, shuffle=True
-        )
+    # Author identification unknown dataset
+    pan18loader_unknown = torch.utils.data.DataLoader(
+        dataset.TIRAAuthorIdentificationDataset(
+            root=args.input_dataset,
+            transform=transformer,
+            problem_name=problem_name,
+            encoding=problem_encoding,
+            train=False
+        ),
+        batch_size=1, shuffle=True
+    )
 
-        # Authors
-        author_to_idx = dict()
-        for idx, author in enumerate(pan18loader_training.dataset.authors):
-            author_to_idx[author] = idx
-        # end for
-
-        # Number of authors
-        n_authors = len(author_to_idx)
-
-        # ESN cell
-        esn = etnn.LiESN(
-            input_dim=transformer.transforms[1].input_dim,
-            hidden_dim=reservoir_size,
-            output_dim=n_authors,
-            spectral_radius=spectral_radius,
-            sparsity=input_sparsity,
-            input_scaling=input_scaling,
-            w_sparsity=w_sparsity,
-            learning_algo='inv',
-            leaky_rate=leak_rate
-        )
-
-        # Get training data for this fold
-        for i, data in enumerate(pan18loader_training):
-            # Inputs and labels
-            inputs, labels = data
-
-            # Create time labels
-            author_id = author_to_idx[labels[0]]
-            tag_vector = torch.zeros(1, inputs.size(1), n_authors)
-            tag_vector[0, :, author_id] = 1.0
-
-            # To variable
-            inputs, time_labels = Variable(inputs), Variable(tag_vector)
-
-            # Accumulate xTx and xTy
-            esn(inputs, time_labels)
-        # end for
-
-        # Finalize training
-        esn.finalize()
-
-        # Counters
-        successes = 0.0
-        count = 0.0
-
-        # Results
-        results = dict()
-
-        # Get test data
-        for i, data in enumerate(pan18loader_unknown):
-            # Inputs and labels
-            inputs, labels = data
-
-            # Author id
-            author_id = author_to_idx[labels[0]]
-
-            # To variable
-            inputs, label = Variable(inputs), Variable(torch.LongTensor([author_id]))
-
-            # Predict
-            y_predicted = esn(inputs)
-
-            # Normalized
-            y_predicted -= torch.min(y_predicted)
-            y_predicted /= torch.max(y_predicted) - torch.min(y_predicted)
-
-            # Sum to one
-            sums = torch.sum(y_predicted, dim=2)
-            for t in range(y_predicted.size(1)):
-                y_predicted[0, t, :] = y_predicted[0, t, :] / sums[0, t]
-            # end for
-
-            # Max average through time
-            """y_predicted = echotorch.utils.max_average_through_time(y_predicted, dim=1)
-    
-            # Compare
-            if torch.equal(y_predicted, labels):
-                successes += 1.0
-            # end if
-            count += 1.0"""
-            # Problem file
-            problem_file = pan18loader_unknown.dataset.last_text
-
-            # Save results
-            results[problem_file] = 1
-        # end for
-
-        # Show accuracy
-        # print(u"Problem {}, Accuracy : {}".format(problem, successes / count * 100.0))
-
-        # Save result in file
-        tools.functions.save_results(
-            os.path.join(args.output_dir, "answers-{}.json".format(pan18loader_unknown.dataset.problem_name)),
-            results)
+    # Authors
+    author_to_idx = dict()
+    idx_to_author = dict()
+    for idx, author in enumerate(pan18loader_training.dataset.authors):
+        author_to_idx[author] = idx
+        idx_to_author[idx] = author
     # end for
+
+    # Number of authors
+    n_authors = len(author_to_idx)
+
+    # ESN cell
+    esn = etnn.BDESN(
+        input_dim=transformer.transforms[1].input_dim,
+        hidden_dim=settings.reservoir_size,
+        output_dim=n_authors,
+        spectral_radius=settings.spectral_radius,
+        sparsity=settings.input_sparsity,
+        input_scaling=settings.input_scaling,
+        w_sparsity=settings.w_sparsity,
+        learning_algo='inv',
+        leaky_rate=settings.leaky_rate,
+        w=torch.load(open('W/' + problem_lang + '.pth', 'rb'))
+    )
+
+    # Get training data for this fold
+    for i, data in enumerate(pan18loader_training):
+        # Inputs and labels
+        inputs, labels = data
+
+        # Create time labels
+        author_id = author_to_idx[labels[0]]
+        tag_vector = torch.zeros(1, inputs.size(1), n_authors)
+        tag_vector[0, :, author_id] = 1.0
+
+        # To variable
+        inputs, time_labels = Variable(inputs), Variable(tag_vector)
+
+        # Accumulate xTx and xTy
+        esn(inputs, time_labels)
+    # end for
+
+    # Finalize training
+    esn.finalize()
+
+    # Results
+    results = dict()
+
+    # Get test data
+    for i, data in enumerate(pan18loader_unknown):
+        # Inputs and labels
+        inputs, _ = data
+
+        # To variable
+        inputs = Variable(inputs)
+
+        # Predict
+        y_predicted = esn(inputs)
+
+        # Normalized
+        y_predicted -= torch.min(y_predicted)
+        y_predicted /= torch.max(y_predicted) - torch.min(y_predicted)
+
+        # Sum to one
+        sums = torch.sum(y_predicted, dim=2)
+        for t in range(y_predicted.size(1)):
+            y_predicted[0, t, :] = y_predicted[0, t, :] / sums[0, t]
+        # end for
+
+        # Max average through time
+        y_predicted = echotorch.utils.max_average_through_time(y_predicted, dim=1)
+
+        # Problem file
+        problem_file = pan18loader_unknown.dataset.last_text
+
+        # Save results
+        results[problem_file] = idx_to_author[y_predicted[0]]
+    # end for
+
+    # Save result in file
+    tools.functions.save_results(
+        os.path.join(args.output_dir, "answers-{}.json".format(problem_name)),
+        results
+    )
 # end for
